cd2c7da92edfc8326aa189692b2941bc80180892
Harish Kumar <harishkumarnit03112002@gmail.com>
2023-07-06 01:00:29 +0530

    added the api and fetched the data from it

--- articlesPage/article.js
+++ articlesPage/article.js
@@ -1,12 +1,37 @@
-// api calling for articles page
+const fetchNews = async (page, q) => {
+  console.log("Fetching News...");
+  var url =
+    "https://newsapi.org/v2/everything?" +
+    "q=" +
+    q +
+    "&from=2023&" +
+    "pageSize=4&" +
+    "language=en&" +
+    "page=" +
+    page +
+    "&sortBy=relevancy&" +
+    "apiKey=fa3f567c121f48feab69aaab3892b152";
 
-async function callApi() {
-  let result = await fetch("");
-  result = await result.json();
-  console.warn(result);
-  document.getElementById("userData").innerHTML = result.map((user) => {
-    ``;
-  });
-}
+  var req = new Request(url);
 
-callApi();
+  let a = await fetch(req);
+  let response = await a.json();
+  console.log(JSON.stringify(response));
+
+  let str = "";
+  for (let item of response.articles) {
+    str =
+      str +
+      `<div class="column">
+      <div class="card">
+        <img src="${item.urlToImage}" alt="..." />
+        <div class="container">
+          <p>${item.description.slice(0, 150) + ".."}</p>
+          <a href="${item.url}" target="_blank" class="btn">Go somewhere</a>
+        </div>
+      </div>
+    </div>`;
+  }
+  document.querySelector(".content").innerHTML = str;
+};
+fetchNews(1, "diseases");
